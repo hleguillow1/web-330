c277d789054c05d0aa0ab3267f13649580e0f199
Hector Leguillow <hfleguillow@me.bellevue.edu>
2018-02-28 16:13:40 -0600

    Adding commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# web-330
-JavaScript II
-Professor Krasso - Bellevue University
-Hector Leguillow - Bellevue University
-
---
+++ Week-2/Leguillow-Exercise-2.2.js
@@ -0,0 +1,59 @@
+/*
+============================================
+; Title: Assignment2.2
+; Author: Professor Krasso
+; Date: 28 February 2018
+; Modified By: Hector Leguillow
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables in an application.
+;===========================================
+
+*//*
+ Expected output:
+
+ FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+
+ The person's full name is '<fullname>.'
+ The person's age is '<age>.'
+
+*/
+
+// start program
+
+
+
+
+
+
+
+
+var person =
+{
+    getAge:function()
+   {
+    return this.age;
+   }
+
+};
+
+var robert = Object.create(person,)
+"age";
+{
+    "value"; "46"
+};
+
+"fullname";
+{
+"value"; "Robert Schumann"
+}
+Robert.getAge();
+
+console.log("The persons's fullname is '%s': robert.fullname); 
+console.log("The person's age is '%s' robert.age)
+
+
+
+// end program
